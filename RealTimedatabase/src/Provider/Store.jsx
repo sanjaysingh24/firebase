7b32ea224d8ae49f3602ffb6c88b6f6b5c2cb1ad
sanju <sanjubora84@gmail.com>
2024-07-29 18:09:32 +0530

    update

---
+++ RealTimedatabase/src/Provider/Store.jsx
@@ -0,0 +1,10 @@
+import {createContext} from 'react';
+
+export const StoreProvider = createContext(null);
+
+export const Store = (props)=>{
+    return(
+        <>
+        </>
+    )
+}
---
+++ RealTimedatabase/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxraWqLLNV9M-aL081Sl0pPbOc-oUoN-g",
+  authDomain: "level-oxygen-427510-e2.firebaseapp.com",
+  projectId: "level-oxygen-427510-e2",
+  storageBucket: "level-oxygen-427510-e2.appspot.com",
+  messagingSenderId: "39191391644",
+  appId: "1:39191391644:web:f2f1a8e3c5e95d2fa7ec59",
+  measurementId: "G-V5Q0FZ0Z9C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
