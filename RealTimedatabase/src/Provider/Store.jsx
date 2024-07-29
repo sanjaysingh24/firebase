800da1ac7c588ad2ee33aa1ae36b5778540be576
sanju <sanjubora84@gmail.com>
2024-07-29 18:19:55 +0530

    update

--- RealTimedatabase/src/Provider/Store.jsx
+++ RealTimedatabase/src/Provider/Store.jsx
@@ -3,8 +3,14 @@ import {createContext} from 'react';
 export const StoreProvider = createContext(null);
 
 export const Store = (props)=>{
+    const baba = "sanju";
+    const contextvalue = {baba};
     return(
         <>
+        <StoreProvider.Provider value={contextvalue}>
+
+            {props.children}
+        </StoreProvider.Provider>
         </>
     )
 }
